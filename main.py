37c9fc9079da6e07ebfc85952765b39e876f3bf3
botkomakers <siam420riiya@gmail.com>
2025-05-19 12:42:02 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import asyncio
-import logging
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+import asyncio, logging
 from config import Config
 from pyrogram import Client as VJ, idle
 from typing import Union, Optional, AsyncGenerator
@@ -10,34 +13,54 @@ from plugins.regix import restart_forwards
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
-VJBot = VJ(
-    "VJ-Forward-Bot",
-    bot_token=Config.BOT_TOKEN,
-    api_id=Config.API_ID,
-    api_hash=Config.API_HASH,
-    sleep_threshold=120,
-    plugins=dict(root="plugins")
-)
+if __name__ == "__main__":
+    VJBot = VJ(
+        "VJ-Forward-Bot",
+        bot_token=Config.BOT_TOKEN,
+        api_id=Config.API_ID,
+        api_hash=Config.API_HASH,
+        sleep_threshold=120,
+        plugins=dict(root="plugins")
+    )  
+    async def iter_messages(
+        self,
+        chat_id: Union[int, str],
+        limit: int,
+        offset: int = 0,
+    ) -> Optional[AsyncGenerator["types.Message", None]]:
+        """Iterate through a chat sequentially.
+        This convenience method does the same as repeatedly calling :meth:`~pyrogram.Client.get_messages` in a loop, thus saving
+        you from the hassle of setting up boilerplate code. It is useful for getting the whole chat messages with a
+        single call.
+        Parameters:
+            chat_id (``int`` | ``str``):
+                Unique identifier (int) or username (str) of the target chat.
+                For your personal cloud (Saved Messages) you can simply use "me" or "self".
+                For a contact that exists in your Telegram address book you can use his phone number (str).
+                
+            limit (``int``):
+                Identifier of the last message to be returned.
+                
+            offset (``int``, *optional*):
+                Identifier of the first message to be returned.
+                Defaults to 0.
+        Returns:
+            ``Generator``: A generator yielding :obj:`~pyrogram.types.Message` objects.
+        Example:
+            .. code-block:: python
+                for message in app.iter_messages("pyrogram", 1, 15000):
+                    print(message.text)
+        """
+        current = offset
+        while True:
+            new_diff = min(200, limit - current)
+            if new_diff <= 0:
+                return
+            messages = await self.get_messages(chat_id, list(range(current, current+new_diff+1)))
+            for message in messages:
+                yield message
+                current += 1
 
-async def iter_messages(
-    self,
-    chat_id: Union[int, str],
-    limit: int,
-    offset: int = 0,
-) -> Optional[AsyncGenerator["types.Message", None]]:
-    current = offset
-    while True:
-        new_diff = min(200, limit - current)
-        if new_diff <= 0:
-            return
-        messages = await self.get_messages(chat_id, list(range(current, current+new_diff+1)))
-        for message in messages:
-            yield message
-            current += 1
-
-async def main():
-    await VJBot.start()
-    bot_info = await VJBot.get_me()
 
     # Send restart message to log channel
     await VJBot.send_message(
@@ -48,9 +71,12 @@ async def main():
              f"**ID:** `{bot_info.id}`"
     )
 
-    await restart_forwards(VJBot)
-    print("Bot Started.")
-    await idle()
 
-if __name__ == "__main__":
-    asyncio.run(main())
+    async def main():
+        await VJBot.start()
+        bot_info  = await VJBot.get_me()
+        await restart_forwards(VJBot)
+        print("Bot Started.")
+        await idle()
+
+    asyncio.get_event_loop().run_until_complete(main())
