a80c4f771a1b26c0616511cdc36d0c0ed7aec177
botkomakers <siam420riiya@gmail.com>
2025-05-19 11:34:50 +0600

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import asyncio
 import logging
 from config import Config
 from pyrogram import Client as VJ, idle
+from typing import Union, Optional, AsyncGenerator
+from logging.handlers import RotatingFileHandler
 from plugins.regix import restart_forwards
 
 # Don't Remove Credit Tg - @VJ_Botz
@@ -16,6 +18,22 @@ VJBot = VJ(
     sleep_threshold=120,
     plugins=dict(root="plugins")
 )
+
+async def iter_messages(
+    self,
+    chat_id: Union[int, str],
+    limit: int,
+    offset: int = 0,
+) -> Optional[AsyncGenerator["types.Message", None]]:
+    current = offset
+    while True:
+        new_diff = min(200, limit - current)
+        if new_diff <= 0:
+            return
+        messages = await self.get_messages(chat_id, list(range(current, current+new_diff+1)))
+        for message in messages:
+            yield message
+            current += 1
 
 async def main():
     await VJBot.start()
