03ba2c3e9b06df71010e323bfc090dd971b465e8
botkomakers <siam420riiya@gmail.com>
2025-05-19 09:49:12 +0600

    Update config.py

--- config.py
+++ config.py
@@ -14,6 +14,9 @@ class Config:
     DATABASE_URI = environ.get("DATABASE_URI", "")
     DATABASE_NAME = environ.get("DATABASE_NAME", "vj-forward-bot")
     BOT_OWNER = int(environ.get("BOT_OWNER", ""))
+
+    USERS_COLLECTION = "collection"
+        
    
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
