6caf25d9775550992df8d7e3718bafcad0761420
botkomakers <siam420riiya@gmail.com>
2025-05-21 21:16:00 +0600

    Update db.py

--- plugins/db.py
+++ plugins/db.py
@@ -1,54 +1,57 @@
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
+import asyncio
 import motor.motor_asyncio
 
-# Don't Remove Credit Tg - @VJ_Botz
+#Boss
+
+
 
 class MongoDB:
-    def __init__(self, uri: str, db_name: str):
+    def __init__(self, uri, db_name, collection):
         self.uri = uri
         self.db_name = db_name
+        self.collection = collection 
         self.client = None
         self.db = None
+        self.files = None
+        self.banned = banned
 
     async def connect(self):
         self.client = motor.motor_asyncio.AsyncIOMotorClient(self.uri)
         self.db = self.client[self.db_name]
+        self.files = self.db[self.collection]
 
     async def close(self):
         if self.client:
             self.client.close()
 
-    def get_collection(self, collection_name: str):
-        return self.db[collection_name]
-
-    async def add_file(self, collection_name: str, file_id):
-        collection = self.get_collection(collection_name)
+    async def add_file(self, file_id):
         file = {"file_id": file_id}
-        return await collection.insert_one(file)
+        return await self.files.insert_one(file)
 
-    async def is_file_exist(self, collection_name: str, file_id):
-        collection = self.get_collection(collection_name)
-        f = await collection.find_one({"file_id": file_id})
+    async def is_file_exit(self, file_id):
+        f = await self.files.find_one({"file_id": file_id})
         return bool(f)
 
-    async def get_all_files(self, collection_name: str):
-        collection = self.get_collection(collection_name)
-        return collection.find({})
+    async def get_all_files(self):
+        return self.files.find({})
 
-    async def drop_all(self, collection_name: str):
-        collection = self.get_collection(collection_name)
-        return await collection.drop()
+    async def drop_all(self):
+        return await self.files.drop()
 
-# Example usage to connect to a user's custom DB and collection
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
 async def connect_user_db(user_id, uri, chat):
     chat = f"{user_id}{chat}"
     dbname = f"{user_id}-Forward-Bot"
-    db = MongoDB(uri, dbname)
+    db = MongoDB(uri, dbname, chat)
     try:
         await db.connect()
     except Exception as e:
-        print(f"Connection error: {e}")
+        print(e)
         return False, db
     return True, db
